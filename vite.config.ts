6b82b243fc583b71c8f1d09b3a3c0da7ebb05359
Jonathan <jonathan.perrinet@epita.fr>
2025-02-18 10:36:00 +0100

    🚑 Add Shadcn UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'corpusense'
+  base: 'corpusense',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
