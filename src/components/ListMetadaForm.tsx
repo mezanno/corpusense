fa741690de6f8b2afee31e64e1d02febac99f9f2
Jonathan <jonathan.perrinet@epita.fr>
2025-03-06 17:03:33 +0100

    🚧 ✨ update metada ui for the lists

---
+++ src/components/ListMetadaForm.tsx
@@ -0,0 +1,183 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { List } from '@/data/models/List';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { updateListRequest } from '@/state/reducers/lists';
+import { addNewTag } from '@/state/reducers/tags';
+import { getTags } from '@/state/selectors/tags';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Tag as FormTag, TagInput } from 'emblor';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { v4 as uuid } from 'uuid';
+import { z } from 'zod';
+import './taginput.css'; //permet d'enlever le background transparent du taginput
+import { Button } from './ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+
+const formSchema = z.object({
+  name: z.string(),
+  tags: z
+    .array(
+      z.object({
+        id: z.string(),
+        text: z.string(),
+      }),
+    )
+    .optional(),
+  about: z.string().optional(),
+});
+
+const ListMetadaForm = ({ list }: { list: List }) => {
+  const dispatch = useAppDispatch();
+  const storedTags = useAppSelector(getTags);
+  //liste des tags existants dans l'application
+  const autoCompleteTags = storedTags.map((tag) => ({
+    id: tag.id,
+    text: tag.label,
+  }));
+
+  //liste des tags de la liste
+  const listTagsDefaultValue: FormTag[] = [];
+  if (list.tags !== undefined) {
+    list.tags.forEach((tagId) => {
+      const tag = storedTags.find((t) => t.id === tagId);
+      if (tag) {
+        listTagsDefaultValue.push({ id: tag.id, text: tag.label });
+      }
+    });
+  }
+  const [tags, setTags] = useState<FormTag[]>(listTagsDefaultValue);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: list.name,
+      about: list.about,
+      tags: listTagsDefaultValue,
+    },
+  });
+
+  const { setValue } = form;
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values.tags);
+
+    const updatedList = { ...list };
+    updatedList.name = values.name;
+    updatedList.about = values.about;
+    if (values.tags !== undefined) {
+      updatedList.tags = values.tags.map((tag) => tag.id);
+    }
+    dispatch(updateListRequest(updatedList));
+  }
+
+  const handleTagAdded = (newTags: FormTag[]) => {
+    //on récupère les tags qui ne sont pas déjà dans la liste (state tags)
+    const diff = newTags.filter((tag) => !tags.some((t) => t.id === tag.id));
+    if (diff.length > 0) {
+      console.log(diff[0]);
+      dispatch(addNewTag({ id: diff[0].id, label: diff[0].text }));
+    }
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='mx-auto flex w-full flex-col gap-2 p-2 md:p-5'
+        >
+          <div className='flex gap-2'>
+            <div className='flex w-1/2 flex-col gap-2'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem className='w-full'>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='ma liste'
+                        type={'text'}
+                        value={field.value}
+                        onChange={(e) => {
+                          const val = e.target.value;
+                          field.onChange(val);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='about'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>About</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        placeholder='A propos de cette liste'
+                        className='resize-none'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name='tags'
+              render={({ field }) => (
+                <FormItem className='flex w-1/2 flex-col items-start'>
+                  <FormLabel className='text-left'>Tags</FormLabel>
+                  <FormControl id='test'>
+                    <TagInput
+                      {...field}
+                      placeholder='Enter a tag'
+                      tags={tags}
+                      enableAutocomplete={true}
+                      autocompleteOptions={autoCompleteTags}
+                      setTags={(newTags) => {
+                        setTags(newTags);
+                        setValue('tags', newTags as [FormTag, ...FormTag[]]);
+                        handleTagAdded(newTags);
+                      }}
+                      generateTagId={() => uuid()}
+                      styleClasses={{ inlineTagsContainer: 'tagInputInlineContainer' }}
+                    />
+                  </FormControl>
+                  <FormDescription>Tags associated to this list</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className='flex w-full items-center justify-end pt-3'>
+            <Button className='rounded-lg' size='sm'>
+              {/* {isPending ? 'Submitting...' : 'Submit'} */}
+              Enregistrer
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default ListMetadaForm;
--- src/pages/ListInspectorPage.tsx
+++ src/pages/ListInspectorPage.tsx
@@ -1,5 +1,95 @@
+import ListMetadaForm from '@/components/ListMetadaForm';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { getActiveList } from '@/state/selectors/lists';
+import { getCanvasById } from '@/state/selectors/storedItems';
+import { IIIFExternalWebResource } from '@iiif/presentation-3';
+import { Thumbnail } from '@samvera/clover-iiif/primitives';
+import { GridStack } from 'gridstack';
+import 'gridstack/dist/gridstack.min.css';
+import { createRef, useEffect, useRef } from 'react';
+
+const GridThumb = ({ canvasId }: { canvasId: string }) => {
+  const canvas = useAppSelector(getCanvasById(canvasId));
+
+  if (canvas === undefined) {
+    return <div aria-errormessage='Error while loading canvas'>Error while loading canvas</div>;
+  }
+  const thumbnail = canvas.thumbnail as IIIFExternalWebResource[];
+
+  return (
+    <Thumbnail
+      thumbnail={thumbnail}
+      style={{ width: '100px', height: '100px', objectFit: 'contain' }}
+      className='w-fit'
+    />
+  );
+};
+
 const ListInspectorPage = () => {
-  return <div>ListInspectorPage</div>;
+  const dispatch = useAppDispatch();
+  const activeList = useAppSelector(getActiveList);
+  const gridRef = useRef(null);
+  const refs = useRef<{ [key: string]: React.RefObject<HTMLDivElement | null> }>({});
+
+  if (activeList?.content) {
+    if (Object.keys(refs.current).length !== activeList.content.length) {
+      activeList.content.forEach(({ canvasId }) => {
+        if (!(canvasId in refs.current)) {
+          refs.current[canvasId] = createRef();
+        }
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (activeList?.content && gridRef.current !== null) {
+      const grid = GridStack.init({ float: false, disableResize: true }, gridRef.current);
+      grid.on('change', (event, items) => {
+        grid.compact();
+      });
+      grid.batchUpdate(); //afin d'éviter les rendus tant qu'on n'a pas terminé les makeWidgets
+      grid.removeAll();
+      activeList.content.forEach(({ canvasId }) => {
+        const item = refs.current[canvasId].current;
+        if (item !== null) {
+          grid.makeWidget(item);
+        }
+      });
+      grid.batchUpdate(false); //on termine les makeWidgets
+    }
+  }, [activeList]);
+
+  return (
+    <div className='flex-col space-y-2'>
+      {activeList && (
+        <>
+          <h1>List Inspector</h1>
+          <div className='rounded-md border bg-white'>
+            <ListMetadaForm list={activeList} />
+          </div>
+          <div>
+            {activeList?.content ? (
+              <div ref={gridRef} className='grid-stack flex h-max rounded-md border bg-white'>
+                {activeList.content.map((item) => (
+                  <div
+                    key={item.canvasId}
+                    ref={refs.current[item.canvasId]}
+                    className='grid-stack-item'
+                  >
+                    <div className='grid-stack-item-content flex items-center justify-center'>
+                      <GridThumb canvasId={item.canvasId} />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>Aucun élément dans la liste</div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ListInspectorPage;
