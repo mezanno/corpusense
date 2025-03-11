b0983ca676e7cc6440c80c67b7136fa7a6a70d5a
Jonathan <jonathan.perrinet@epita.fr>
2025-03-11 16:55:05 +0100

    ⚡️ added memorizing to selectors

--- src/state/selectors/lists.ts
+++ src/state/selectors/lists.ts
@@ -1,15 +1,25 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
 export const getLists = (state: RootState) => state?.lists?.values ?? [];
 
-export const getActiveList = (state: RootState) =>
-  state.lists.values.find((elt) => elt.id === state.lists.activeListId);
+export const getActiveList = createSelector(
+  [(state: RootState) => state.lists.values, (state: RootState) => state.lists.activeListId],
+  (values, activeListId) => values.find((elt) => elt.id === activeListId),
+);
 
-export const getElemntsOfList = (listId: string) => (state: RootState) => {
-  return state.lists.values.find((elt) => elt.id === listId)?.content ?? [];
-};
+export const getElemntsOfList = createSelector(
+  [(state: RootState) => state.lists?.values ?? [], (_: RootState, listId: string) => listId],
+  (values, listId) => values.find((elt) => elt.id === listId)?.content ?? [],
+);
 
-export const getCanvasesOfList = (listId: string) => (state: RootState) => {
-  const elementsIds = getElemntsOfList(listId)(state).map((elt) => elt.canvasId);
-  return state.storedItems.items.filter((elt) => elementsIds.includes(elt.id));
-};
+export const getCanvasesOfList = createSelector(
+  [
+    (state: RootState, listId: string) => getElemntsOfList(state, listId),
+    (state: RootState) => state.storedItems?.items ?? [],
+  ],
+  (elements, items) => {
+    const elementsIds = new Set(elements.map((elt) => elt.canvasId));
+    return items.filter((elt) => elementsIds.has(elt.id));
+  },
+);
--- src/state/selectors/storedItems.ts
+++ src/state/selectors/storedItems.ts
@@ -1,4 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
-export const getCanvasById = (canvasId: string) => (state: RootState) =>
-  state.storedItems.items.find((elt) => elt.id === canvasId)?.content;
+export const getCanvasById = createSelector(
+  [(state: RootState) => state.storedItems.items, (_: RootState, canvasId: string) => canvasId],
+  (items, canvasId) => items.find((elt) => elt.id === canvasId)?.content,
+);
+
+// (canvasId: string) => (state: RootState) =>
+// state.storedItems.items.find((elt) => elt.id === canvasId)?.content;
