41d909013135430fe48b5c79b7a20e0524ebb1ec
Jonathan <jonathan.perrinet@epita.fr>
2025-03-21 13:58:14 +0100

    🔥 remove OCR from the page level

--- src/pages/ManifestExplorerPage.tsx
+++ src/pages/ManifestExplorerPage.tsx
@@ -1,22 +1,16 @@
 import CanvasViewer from '@/components/CanvasViewer';
-import { Progress } from '@/components/ui/progress';
 import { Toggle } from '@/components/ui/toggle';
 import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
 import { fetchManifestFromUrlRequest } from '@/state/reducers/manifests';
-import { getCanvasForCanvas } from '@/state/selectors/canvas';
 import { Annotorious } from '@annotorious/react';
 import { PanelTopClose, PanelTopOpen } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { createWorker } from 'tesseract.js';
 import CanvasGallery from '../components/CanvasGallery';
 import ManifestDetails from '../components/ManifestDetails';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '../components/ui/resizable';
 
 const ManifestExplorerPage = () => {
-  const canvasImage = useAppSelector(getCanvasForCanvas('test'));
-  const [progress, setProgress] = useState(0);
-  const [working, setWorking] = useState(false);
   const { isLoading, isLoaded } = useAppSelector((state) => state.manifests);
   const [isMetadataOpen, setIsMetadataOpen] = useState(true);
   const [isGalleryOpen, setIsGalleryOpen] = useState(true);
@@ -31,24 +25,6 @@ const ManifestExplorerPage = () => {
       dispatch(fetchManifestFromUrlRequest(id));
     }
   }, [searchParams]);
-
-  const handleGetOcr = async () => {
-    if (!canvasImage) return;
-
-    setWorking(true);
-    const worker = await createWorker('fra', 1, {
-      logger: (m) => {
-        setProgress(m.progress * 100);
-      },
-    });
-    console.log('OCR ', canvasImage);
-    const {
-      data: { text },
-    } = await worker.recognize(canvasImage.id);
-    console.log(text);
-    await worker.terminate();
-    setWorking(false);
-  };
 
   return (
     <main className='relative h-full w-full'>
@@ -103,18 +79,6 @@ const ManifestExplorerPage = () => {
               <Annotorious>
                 <CanvasViewer />
               </Annotorious>
-              <button
-                className='absolute top-0 right-0 m-4 rounded-md bg-white p-2 shadow-md'
-                // eslint-disable-next-line @typescript-eslint/no-misused-promises
-                onClick={handleGetOcr}
-              >
-                OCR
-              </button>
-              {working && (
-                <div className='absolute top-0 left-0 flex h-full w-full items-center justify-center'>
-                  <Progress value={progress} className='w-[60%]' />
-                </div>
-              )}
             </ResizablePanel>
           </>
         )}
