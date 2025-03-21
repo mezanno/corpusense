8aa039cad96badbfc3ca63101aa6c46e76014079
Jonathan <jonathan.perrinet@epita.fr>
2025-03-21 15:46:13 +0100

    🔥 remove listElements table

--- src/data/db.ts
+++ src/data/db.ts
@@ -2,7 +2,6 @@ import Dexie, { type EntityTable } from 'dexie';
 import { Annotation } from './models/Annotation';
 import { History } from './models/History';
 import { List } from './models/List';
-import { ListElement } from './models/ListElement';
 import { ItemMetadata } from './models/Metadata';
 import { StoredItem } from './models/StoredItem';
 import { Tag } from './models/Tag';
@@ -11,7 +10,7 @@ const db = new Dexie('mezanno') as Dexie & {
   lists: EntityTable<List, 'id'>;
   history: EntityTable<History, 'url'>;
   storedItems: EntityTable<StoredItem, 'id'>;
-  listElements: EntityTable<ListElement, 'id'>;
+  // listElements: EntityTable<ListElement, 'id'>;
   itemMetadata: EntityTable<ItemMetadata, 'id'>;
   tags: EntityTable<Tag, 'id'>;
   annotations: EntityTable<Annotation, 'id'>;
@@ -21,7 +20,7 @@ db.version(1).stores({
   lists: '&id, name, *tags.id',
   history: '&url',
   storedItems: '&id',
-  listElements: '++id, canvasId, listId',
+  // listElements: '++id, canvasId, listId, [listId+canvasId]',
   typesList: '&label',
   itemMetadata: '[id+attribute.label]',
   tags: '&id',
