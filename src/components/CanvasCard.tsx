d40d96a431e68952ea84c19529348a532c97fe6a
Jonathan <jonathan.perrinet@epita.fr>
2025-03-21 14:00:11 +0100

    ✨ add menu to create list from selection

--- src/components/CanvasCard.tsx
+++ src/components/CanvasCard.tsx
@@ -1,10 +1,11 @@
 import { Canvas, IIIFExternalWebResource } from '@iiif/presentation-3';
-import { Label, Thumbnail } from '@samvera/clover-iiif/primitives';
+import { Label as LabelIif, Thumbnail } from '@samvera/clover-iiif/primitives';
 import { Card, CardContent, CardHeader } from './ui/card';
 import {
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
+  ContextMenuSeparator,
   ContextMenuSub,
   ContextMenuSubContent,
   ContextMenuSubTrigger,
@@ -13,10 +14,24 @@ import {
 
 import { List } from '@/data/models/List';
 import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
-import { addSelectionToListRequest } from '@/state/reducers/lists';
+import { addSelectionToListRequest, createListWithSelectionRequest } from '@/state/reducers/lists';
 import { setSelectionEndRequest, setSelectionStartRequest } from '@/state/reducers/selection';
 import { getLists } from '@/state/selectors/lists';
+import { useRef } from 'react';
 import { getSelection, isSelected } from '../state/selectors/selection';
+import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 interface CanvasCardProps {
   index: number;
@@ -28,6 +43,8 @@ const CanvasCard = ({ index, canvas, onClick }: CanvasCardProps) => {
   const selection = useAppSelector(getSelection);
   const lists: List[] = useAppSelector(getLists);
   const selected: boolean = useAppSelector(isSelected(index, canvas.id));
+
+  const inputListName = useRef(null);
 
   //TODO : gérer le cas où canvas.thumbnail est undefined
   const thumbnail = canvas.thumbnail as IIIFExternalWebResource[];
@@ -65,56 +82,101 @@ const CanvasCard = ({ index, canvas, onClick }: CanvasCardProps) => {
     }
   };
 
-  return (
-    <ContextMenu>
-      <div className='flex h-full w-full justify-center'>
-        <ContextMenuTrigger>
-          <Card
-            onClick={(e) => handleClick(e.target)}
-            className={`selectable-item h-fit w-fit ${selected ? 'bg-blue-300' : 'bg-white'}`}
-            data-index={index}
-            data-canvas-id={canvas.id}
-            role='listitem'
-          >
-            <CardHeader>
-              <Label className='text-center' label={canvas.label ? canvas.label : {}} />
-            </CardHeader>
-            <CardContent>
-              <Thumbnail
-                thumbnail={thumbnail}
-                style={{ width: 'auto', height: '100px', objectFit: 'contain' }}
-                className='w-fit'
-              />
-            </CardContent>
-          </Card>
-        </ContextMenuTrigger>
-      </div>
+  const handleCreateList = () => {
+    const listName: string = inputListName?.current?.value;
+    dispatch(createListWithSelectionRequest({ selection, name: listName }));
+  };
 
-      <ContextMenuContent>
-        {selection.length > 0 && (
-          <ContextMenuSub>
-            <ContextMenuSubTrigger> Ajouter la sélection à une liste</ContextMenuSubTrigger>
-            <ContextMenuSubContent>
-              {lists?.length > 0 &&
-                lists.map((list: List) => (
-                  <ContextMenuItem key={list.id} onClick={() => handleAddSelectionToList(list.id)}>
-                    {list.name}
-                  </ContextMenuItem>
-                ))}
-            </ContextMenuSubContent>
-          </ContextMenuSub>
-        )}
-        <ContextMenuItem onClick={handleSetSelectionStart}>
-          Définir le début de sélection ici
-        </ContextMenuItem>
-        <ContextMenuItem onClick={handleSetSelectionEnd}>
-          Définir la fin de sélection ici
-        </ContextMenuItem>
-        <ContextMenuItem onClick={handleCopyToClipboard}>
-          Copier l&apos;URL de la ressource dans le presse-papier
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+  return (
+    <Dialog>
+      <ContextMenu>
+        <div className='flex h-full w-full justify-center'>
+          <ContextMenuTrigger>
+            <Card
+              onClick={(e) => handleClick(e.target)}
+              className={`selectable-item h-fit w-fit ${selected ? 'bg-blue-300' : 'bg-white'}`}
+              data-index={index}
+              data-canvas-id={canvas.id}
+              role='listitem'
+            >
+              <CardHeader>
+                <LabelIif className='text-center' label={canvas.label ? canvas.label : {}} />
+              </CardHeader>
+              <CardContent>
+                <Thumbnail
+                  thumbnail={thumbnail}
+                  style={{ width: 'auto', height: '100px', objectFit: 'contain' }}
+                  className='w-fit'
+                />
+              </CardContent>
+            </Card>
+          </ContextMenuTrigger>
+        </div>
+
+        <ContextMenuContent>
+          {selection.length > 0 && (
+            <>
+              <DialogTrigger asChild>
+                <ContextMenuItem>Créer une liste à partir de la sélection</ContextMenuItem>
+              </DialogTrigger>
+              <ContextMenuSub>
+                <ContextMenuSubTrigger> Ajouter la sélection à une liste</ContextMenuSubTrigger>
+                <ContextMenuSubContent>
+                  {lists?.length > 0 &&
+                    lists.map((list: List) => (
+                      <ContextMenuItem
+                        key={list.id}
+                        onClick={() => handleAddSelectionToList(list.id)}
+                      >
+                        {list.name}
+                      </ContextMenuItem>
+                    ))}
+                </ContextMenuSubContent>
+              </ContextMenuSub>
+
+              <ContextMenuSeparator />
+            </>
+          )}
+          <ContextMenuItem onClick={handleSetSelectionStart}>
+            Définir le début de sélection ici
+          </ContextMenuItem>
+          <ContextMenuItem onClick={handleSetSelectionEnd}>
+            Définir la fin de sélection ici
+          </ContextMenuItem>
+          <ContextMenuSeparator />
+          <ContextMenuItem onClick={handleCopyToClipboard}>
+            Copier l&apos;URL de la ressource dans le presse-papier
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Créer une nouvelle liste</DialogTitle>
+          <DialogDescription>
+            Créez une nouvelle liste à partir de la sélection que vous avez effectuée.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='grid grid-cols-4 items-center gap-4'>
+          <Label htmlFor='name' className='text-right'>
+            Nom de la liste
+          </Label>
+          <Input
+            ref={inputListName}
+            id='name'
+            placeholder='Une liste pas comme les autres'
+            className='col-span-3'
+          />
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type='button' onClick={handleCreateList}>
+              Créer la liste
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
