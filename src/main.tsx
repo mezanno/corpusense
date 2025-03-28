579bfe7950beb8cc989a23d119c61c7943c37bb4
Jonathan <jonathan.perrinet@epita.fr>
2025-03-28 19:10:32 +0100

    🌐 display fallback while loading translations

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
+import './i18n';
 import store from './state/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />,
-    </Provider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Provider store={store}>
+        <App />,
+      </Provider>
+    </Suspense>
   </StrictMode>,
 );
