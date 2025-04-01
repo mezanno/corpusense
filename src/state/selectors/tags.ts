423b6478d2a3fa1984197af5f240452c0e4ed48d
Jonathan <jonathan.perrinet@epita.fr>
2025-04-01 15:33:06 +0200

    ✨ add a function to get all the tags with an array of ids

--- src/state/selectors/tags.ts
+++ src/state/selectors/tags.ts
@@ -1,3 +1,6 @@
 import { RootState } from '../store';
 
 export const getTags = (state: RootState) => state.tags.values;
+
+export const getTagsByIds = (ids: string[]) => (state: RootState) =>
+  ids?.map((id) => state.tags.values.find((t) => t.id === id)).filter(Boolean) ?? [];
