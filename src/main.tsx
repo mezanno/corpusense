9d3821b78a825e0821c7b3e5ac8d51dc19a07b94
Jonathan <jonathan.perrinet@epita.fr>
2025-04-03 18:11:04 +0200

    💄 add css file import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import './App.css';
 import App from './App.tsx';
 import './i18n';
 import store from './state/store.ts';
