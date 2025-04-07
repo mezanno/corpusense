7b9ee597ae83adddc55e07730e23d4793a4f7292
Jonathan <jonathan.perrinet@epita.fr>
2025-04-07 11:12:54 +0200

    change null to empty string

--- src/state/selectors/manifests.ts
+++ src/state/selectors/manifests.ts
@@ -2,8 +2,7 @@ import { RootState } from '../store';
 
 export const getLoadedManifest = (state: RootState) => state?.manifests.loadedData ?? null;
 
-export const getManifestURL = (state: RootState) =>
-  state?.manifests?.loadedData?.content?.id ?? null;
+export const getManifestURL = (state: RootState) => state?.manifests?.loadedData?.content?.id ?? '';
 
 export const getHistory = (state: RootState) => state?.manifests?.history ?? [];
 
