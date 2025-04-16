cec121910046e1ec75c12a52cae8470910fa152c
Jonathan <jonathan.perrinet@epita.fr>
2025-04-16 15:09:57 +0200

    📝 add documentation (manifests)

--- src/state/selectors/manifests.ts
+++ src/state/selectors/manifests.ts
@@ -1,9 +1,21 @@
 import { RootState } from '../store';
 
+/**
+ * @returns The content of the loaded manifest (+ metadata) or null if no manifest is loaded
+ */
 export const getLoadedManifest = (state: RootState) => state?.manifests.loadedData ?? null;
 
+/**
+ * @returns The manifest ID of the loaded manifest or an empty string if no manifest is loaded
+ */
 export const getManifestURL = (state: RootState) => state?.manifests?.loadedData?.content?.id ?? '';
 
+/**
+ * @returns The history of loaded manifests or an empty array if no manifest is loaded
+ */
 export const getHistory = (state: RootState) => state?.manifests?.history ?? [];
 
+/**
+ * @returns The canvases of the loaded manifest or an empty array if no manifest is loaded
+ */
 export const getCanvases = (state: RootState) => state?.manifests?.loadedData?.content?.items ?? [];
