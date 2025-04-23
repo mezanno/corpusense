a868490cf91921ff0dd56cd24488188c73908787
Jonathan <jonathan.perrinet@epita.fr>
2025-04-23 22:41:28 +0200

    ✨ add multioptionsmenu customizable

---
+++ src/components/MultiOptionsMenu.tsx
@@ -0,0 +1,63 @@
+import { useTranslation } from 'react-i18next';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Spinner } from './ui/spinner';
+
+export interface MultiOptionsMenuParams {
+  name: string;
+  icon: React.ReactNode;
+  info: string;
+  items: {
+    name: string;
+    icon: React.ReactNode;
+    action: () => void;
+  }[];
+}
+
+const MultiOptionsMenu = ({
+  params,
+  isRunning,
+}: {
+  params: MultiOptionsMenuParams;
+  isRunning: boolean;
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className='relative'>
+      <DropdownMenu>
+        <DropdownMenuTrigger className='soft-button' title={t(params.name)}>
+          {params.icon}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className='flex items-center gap-2 space-x-2'>
+            {isRunning ? (
+              <>
+                {t('info_analysis_running')}
+                <Spinner size={'small'} />
+              </>
+            ) : (
+              t(params.info)
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {params.items.map((item, index) => (
+            <DropdownMenuItem key={index} disabled={isRunning} onClick={item.action}>
+              {item.icon}
+              {t(item.name)}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {isRunning && <Spinner className='absolute -top-2 -right-2' size={'small'} />}
+    </div>
+  );
+};
+
+export default MultiOptionsMenu;
