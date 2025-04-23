a823389bf49451b724fb259b86578680c886b6fb
Jonathan <jonathan.perrinet@epita.fr>
2025-04-23 22:42:05 +0200

    ♻️ refactor menu

--- src/components/AnalysisMenu.tsx
+++ src/components/AnalysisMenu.tsx
@@ -1,14 +1,5 @@
 import { ScanText, TableProperties, TextSearch } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-import { Spinner } from './ui/spinner';
+import MultiOptionsMenu from './MultiOptionsMenu';
 
 const AnalysisMenu = ({
   isRunning,
@@ -19,38 +10,25 @@ const AnalysisMenu = ({
   handleLayout: () => void;
   handleOcr: () => void;
 }) => {
-  const { t } = useTranslation();
-  return (
-    <div className='relative'>
-      <DropdownMenu>
-        <DropdownMenuTrigger className='soft-button' title={t('btn_start_analysis')}>
-          <TextSearch />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel className='flex items-center gap-2 space-x-2'>
-            {isRunning ? (
-              <>
-                {t('info_analysis_running')}
-                <Spinner size={'small'} />
-              </>
-            ) : (
-              t('info_start_analysis')
-            )}
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem disabled={isRunning} onClick={handleLayout}>
-            <TableProperties />
-            {t('btn_detect_layout')}
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled={isRunning} onClick={handleOcr}>
-            <ScanText />
-            {t('btn_OCR_analyze')}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      {isRunning && <Spinner className='absolute -top-2 -right-2' size={'small'} />}
-    </div>
-  );
+  const params = {
+    name: 'btn_analysis_menu',
+    icon: <TextSearch />,
+    info: 'info_start_analysis',
+    items: [
+      {
+        name: 'btn_detect_layout',
+        icon: <TableProperties />,
+        action: handleLayout,
+      },
+      {
+        name: 'btn_OCR_analyze',
+        icon: <ScanText />,
+        action: handleOcr,
+      },
+    ],
+  };
+
+  return <MultiOptionsMenu params={params} isRunning={isRunning} />;
 };
 
 export default AnalysisMenu;
