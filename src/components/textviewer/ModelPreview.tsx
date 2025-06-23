bb3d06bd3e94b6f7987751012b01d71e510274a6
Jonathan <jonathan.perrinet@epita.fr>
2025-06-23 18:08:40 +0200

    ⚡️  update ModelPreview to use generateSchema instead of generatePreview for improved schema representation

--- src/components/textviewer/ModelPreview.tsx
+++ src/components/textviewer/ModelPreview.tsx
@@ -1,9 +1,9 @@
 import { DataModel } from '@/data/models/DataModel';
-import { generatePreview } from '@/data/utils/model';
+import { generateSchema } from '@/data/utils/model';
 
 //@ts-expect-error close is unused
 const ModelPreview = ({ close, model }: { close: () => void; model: DataModel }) => {
-  return <div>{generatePreview(model)}</div>;
+  return <div>{JSON.stringify(generateSchema(model))}</div>;
 };
 
 export default ModelPreview;
