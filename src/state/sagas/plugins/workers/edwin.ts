f2cceea47770f14338737a7ca57df611dedf9210
Jonathan <jonathan.perrinet@epita.fr>
2025-09-03 15:14:16 +0200

    ⚡️ refactor worker sagas to use async/await and update return types for better readability and consistency

--- src/state/sagas/plugins/workers/edwin.ts
+++ src/state/sagas/plugins/workers/edwin.ts
@@ -1,4 +1,3 @@
-import { Annotation } from '@/data/models/Annotation';
 import { convertEdwinResult, EdwinBox } from '@/data/models/converters/edwinMagic';
 import { Task, WorkerResponse, WorkerStatus } from '@/data/models/Worker';
 import { getAnnotationRepository } from '@/data/repositories/indexeddb/dbFactory';
@@ -6,45 +5,37 @@ import { getImage } from '@/data/utils/canvas';
 import { addAnnotationsSuccess } from '@/state/reducers/annotations';
 import { PluginParams } from '@/state/reducers/workers';
 import { getErrorMessage } from '@/utils/utils';
-import { PredictReturn } from 'node_modules/@gradio/client/dist/types';
-import { call, Effect, put } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 
 export const pluginName = 'edwin';
 
-export default function* edwinSaga(
+export default async function edwinSaga(
   task: Task,
   _params: PluginParams,
-): Generator<Effect, WorkerResponse, Response | PredictReturn | Annotation[]> {
+): Promise<WorkerResponse> {
   try {
     const image = getImage(task.canvas);
 
-    const response: Response = (yield call(
-      fetch,
-      // `http://localhost:3000/layout?image_url=${imageUrl}`,
-      `https://api.mezanno.xyz/layout?image_url=${image.id}`,
-    )) as Response;
+    const response: Response = await fetch(`https://api.mezanno.xyz/layout?image_url=${image.id}`);
     if (!response.ok) {
       return {
         status: WorkerStatus.ERROR,
         statusMessage: 'Network response was not ok',
       };
     }
-    const data = yield call([response, 'json']);
+    const data: unknown = await response.json();
     console.log('data: ', data);
     //convert the result into an array of Annotation
     const annotations = convertEdwinResult(
-      data as unknown as EdwinBox[],
+      data as EdwinBox[],
       task.canvas.id,
       task.scope.collectionId,
       task.canvas.width ?? 0,
     );
     //and send it to the redux store
     const annotationRepository = getAnnotationRepository();
-    const newAnnotations = (yield call(
-      [annotationRepository, annotationRepository.saveAllAnnotations],
-      annotations,
-    )) as Annotation[];
-    yield put(addAnnotationsSuccess(newAnnotations));
+    const newAnnotations = await annotationRepository.saveAllAnnotations(annotations);
+    put(addAnnotationsSuccess(newAnnotations));
   } catch (error) {
     return {
       status: WorkerStatus.ERROR,
