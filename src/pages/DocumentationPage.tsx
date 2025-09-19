f64b6ad7315151d2b46641d1e710683e342feae8
Jonathan <jonathan.perrinet@epita.fr>
2025-09-19 11:20:35 +0200

    ✨ add remark-gfm for GitHub Flavored Markdown support and update DocumentationPage styling with @tailwindcss/typography

--- src/pages/DocumentationPage.tsx
+++ src/pages/DocumentationPage.tsx
@@ -1,10 +1,11 @@
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import useCaseContent from '/docs/usecase.md?raw';
 
 const DocumentationPage = () => {
   return (
-    <div>
-      <ReactMarkdown>{useCaseContent}</ReactMarkdown>
+    <div className='mx-auto prose p-4'>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{useCaseContent}</ReactMarkdown>
     </div>
   );
 };
