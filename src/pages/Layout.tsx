9775b59e8885a31b464536a8f5f933417f063683
Jonathan <jonathan.perrinet@epita.fr>
2025-09-22 13:26:45 +0200

    🐛 fix layout sizing issue

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -48,9 +48,8 @@ const Layout = () => {
   return (
     <SidebarProvider>
       <LayoutSideBar setSelectedWorkerId={setSelectedWorkerId} />
-      <SidebarInset className='m-2 h-screen w-full'>
-        {/*TODO: Fix this width : pour une raison inconnue w-100 empêche la fenêtre de déborder*/}
-        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+      <SidebarInset className='m-2 flex h-screen min-w-0 flex-1 flex-col'>
+        <header className='flex shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
           <div className='flex items-center space-x-2'>
             <SidebarTrigger />
             <ManifestExplorerDrawer />
@@ -58,7 +57,9 @@ const Layout = () => {
             <ContactDrawer />
           </div>
         </header>
-        <Outlet />
+        <main className='flex-1'>
+          <Outlet />
+        </main>
         <Toaster position='top-right' expand={true} richColors />
       </SidebarInset>
     </SidebarProvider>
