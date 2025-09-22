e28f588de3c952ffc856bb693fed83480a615c3f
Jonathan <jonathan.perrinet@epita.fr>
2025-09-22 10:54:55 +0200

    🐛 fix manifest loading crash sometimes with bnf api v3

--- src/state/sagas/plugins/importers/gallica.ts
+++ src/state/sagas/plugins/importers/gallica.ts
@@ -1,31 +1,42 @@
-import i18n from '@/i18n';
-
 export const pluginName = 'bnf.fr';
 
 const gallicaImporter = async (url: string): Promise<object> => {
   console.log('gallicaImporter: ', url);
-  const fetchUrl = url.replace('gallica.bnf.fr/iiif', 'openapi.bnf.fr/iiif/presentation/v3');
-  const response = await fetch(fetchUrl, {
+  try {
+    const urlV3 = url.replace('gallica.bnf.fr/iiif', 'openapi.bnf.fr/iiif/presentation/v3');
+    return await fetchUrl(urlV3);
+  } catch (error) {
+    // try {
+    return await fetchUrl(url);
+    // } catch (err) {
+    // throw err;
+    // }
+  }
+
+  // if (!response.ok) {
+  //   if (response.status === 404) {
+  //     console.log(i18n.t('error_404_manifest'));
+
+  //     throw new Error(i18n.t('error_404_manifest'));
+  //   } else if (response.status === 403) {
+  //     throw new Error(i18n.t('error_403_manifest'));
+  //   } else {
+  //     throw new Error(`Failed to fetch manifest ${response.statusText}`);
+  //   }
+};
+
+const fetchUrl = async (url: string): Promise<object> => {
+  const response = await fetch(url, {
     // mode: 'no-cors', //ne sert à rien (renvoie 200 mais corps de la réponse vide)
     headers: {
       Accept: 'application/json',
     },
   });
-  if (!response.ok) {
-    if (response.status === 404) {
-      console.log(i18n.t('error_404_manifest'));
-
-      throw new Error(i18n.t('error_404_manifest'));
-    } else if (response.status === 403) {
-      throw new Error(i18n.t('error_403_manifest'));
-    } else {
-      throw new Error(`Failed to fetch manifest ${response.statusText}`);
-    }
+  if (response.ok) {
+    //TODO! gérer cas où ce n'est pas un objet (unknown)
+    return (await response.json()) as object;
   }
-  //TODO! gérer cas où ce n'est pas un objet (unknown)
-  const data: object = (await response.json()) as object;
-
-  return data;
+  throw new Error(`Failed to fetch manifest ${response.statusText}`);
 };
 
 export default gallicaImporter;
