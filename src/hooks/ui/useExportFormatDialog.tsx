d8a15f68c5d38d8247ea90d441a6dc27c864acc5
Jonathan <jonathan.perrinet@epita.fr>
2025-09-23 17:52:51 +0200

    ✨ add useExportFormatDialog hook to manage export format selection dialog

---
+++ src/hooks/ui/useExportFormatDialog.tsx
@@ -0,0 +1,24 @@
+import ExportFormatSelectionForm from '@/components/ExportFormatSelectionForm';
+import { usePopupContext } from '@/components/reducers/usePopupContext';
+import { Worker } from '@/data/models/Worker';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const useExportFormatDialog = () => {
+  const { t } = useTranslation();
+  const { showPopup } = usePopupContext();
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const openSelectFormatDialog = (worker: Worker) => {
+    showPopup({
+      title: t('title_export_worker_result', { name: worker.name }),
+      children: <ExportFormatSelectionForm worker={worker} formRef={formRef} />,
+      onConfirm() {
+        //! La fenêtre se ferme automatiquement, on doit donc déclencher la validation du formulaire manuellement
+        formRef.current?.requestSubmit();
+      },
+      onConfirmMessage: t('btn_export'),
+    });
+  };
+
+  return { openSelectFormatDialog };
+};
