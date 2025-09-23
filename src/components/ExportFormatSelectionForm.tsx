6db04a1d6cf9bf59aba6b8c698856e5736980bc5
Jonathan <jonathan.perrinet@epita.fr>
2025-09-23 17:52:38 +0200

    ✨ add ExportFormatSelectionForm component for selecting export formats

---
+++ src/components/ExportFormatSelectionForm.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Worker } from '@/data/models/Worker';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { exportWorkerResultRequest } from '@/state/reducers/workers';
+import { selectExportFormats } from '@/state/selectors/workers';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Ref } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+import { Checkbox } from './ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+
+const ExportFormatSelectionForm = ({
+  worker,
+  formRef,
+}: {
+  worker: Worker;
+  formRef: Ref<HTMLFormElement | null>;
+}) => {
+  const { t } = useTranslation();
+  const appDispatch = useAppDispatch();
+
+  const formats = useAppSelector((state) => selectExportFormats(state, worker.name));
+
+  const checkbox = formats.reduce(
+    (acc, f) => {
+      acc[f] = z.boolean().optional();
+      return acc;
+    },
+    {} as Record<string, z.ZodOptional<z.ZodBoolean>>,
+  );
+
+  const schema = z.object(checkbox).refine((data) => Object.values(data).some(Boolean), {
+    message: t('error_no_export_format_selected'),
+  });
+
+  const form = useForm<Record<string, boolean | undefined>>({
+    resolver: zodResolver(schema),
+    defaultValues: formats.reduce((acc, f) => ({ ...acc, [f]: false }), {}),
+  });
+
+  const onSubmit = (values: z.infer<typeof schema>) => {
+    const selectedFormats = Object.entries(values)
+      .filter(([, v]) => v === true)
+      .map(([k]) => k);
+    appDispatch(exportWorkerResultRequest({ worker, formats: selectedFormats }));
+  };
+
+  return (
+    <Form {...form}>
+      <FormDescription> {t('description_select_export_formats')}</FormDescription>
+      <form
+        className='flex flex-col items-center space-y-2'
+        onSubmit={form.handleSubmit(onSubmit)}
+        ref={formRef}
+      >
+        {formats.map((format) => (
+          <FormField
+            control={form.control}
+            name={format}
+            key={format}
+            render={({ field }) => (
+              <FormItem className='flex'>
+                <FormControl>
+                  <Checkbox id={format} checked={field.value} onCheckedChange={field.onChange} />
+                </FormControl>
+                <FormLabel>{format}</FormLabel>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+      </form>
+    </Form>
+  );
+};
+
+export default ExportFormatSelectionForm;
