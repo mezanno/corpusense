bab937d4759188e4172ddecf55f0e4cb27a8effa
Jonathan <jonathan.perrinet@epita.fr>
2025-09-26 10:58:10 +0200

    ⚡️  enhance: update file input handling in ImportModelForm and improve form validation using zod

--- src/components/forms/ImportModelForm.tsx
+++ src/components/forms/ImportModelForm.tsx
@@ -1,59 +1,79 @@
 import { useAppDispatch } from '@/hooks/hooks';
 import { importModelRequest } from '@/state/reducers/models';
-import { Ref, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import i18next from 'i18next';
+import { Ref } from 'react';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '../ui/form';
 import { Input } from '../ui/input';
-import { Label } from '../ui/label';
+
+const schema = z.object({
+  file: z
+    .instanceof(File)
+    .refine(
+      (file) => ['application/json'].includes(file.type),
+      i18next.t('error_unsupported_file_type', { types: '.json' }),
+    ),
+});
 
 const ImportModelForm = ({ formRef }: { formRef: Ref<HTMLFormElement | null> }) => {
   const { t } = useTranslation();
   const appDispatch = useAppDispatch();
-  const [file, setFile] = useState<File | null>(null);
-  const [error, setError] = useState<string | null>(null);
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-    }
-  };
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
 
-  const handleImport = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const content = e.target?.result;
-        if (typeof content === 'string') {
-          try {
-            const jsonContent = JSON.parse(content) as object;
-            appDispatch(importModelRequest(jsonContent));
-            console.log(jsonContent);
-            close();
-          } catch (err) {
-            console.error('Error parsing JSON:', err);
-          }
-        } else {
-          setError(t('error_unsupported_file_type'));
+  function onSubmit(values: z.infer<typeof schema>) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const content = e.target?.result;
+      if (typeof content === 'string') {
+        try {
+          const jsonContent = JSON.parse(content) as object;
+          appDispatch(importModelRequest(jsonContent));
+          console.log(jsonContent);
+          close();
+        } catch (err) {
+          console.error('Error parsing JSON:', err);
         }
-      };
-      if (file.name.endsWith('.zip')) {
-        reader.readAsArrayBuffer(file);
-      } else {
-        reader.readAsText(file);
       }
-    }
-  };
+    };
+    reader.readAsText(values.file);
+  }
 
   return (
-    <form
-      ref={formRef}
-      onSubmit={handleImport}
-      className='flex flex-col items-center gap-1.5 rounded-2xl border-2 border-gray-200 p-2'
-    >
-      <Label htmlFor='collectionFile'>Fichier</Label>
-      <Input id='collectionFile' type='file' onChange={handleFileChange} />
-      {error !== null && <div className='text-red-500'>{error}</div>}
-    </form>
+    <Form {...form}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={form.handleSubmit(onSubmit)}
+        ref={formRef}
+        className='space-y-4'
+      >
+        <FormDescription>{t('form_description_select_model')}</FormDescription>
+        <FormField
+          control={form.control}
+          name='file'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type='file'
+                  accept='application/json'
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    field.onChange(file);
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 };
 
