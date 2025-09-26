c209c0a97b494026330001b30087fecb0d761edb
Jonathan <jonathan.perrinet@epita.fr>
2025-09-26 11:06:12 +0200

    ⚡️ improve file upload handling in UploadFileForm with zod validation and refactor form structure

--- src/components/forms/UploadFileForm.tsx
+++ src/components/forms/UploadFileForm.tsx
@@ -1,60 +1,85 @@
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { useAppDispatch } from '@/hooks/hooks';
 import { importCollectionRequest, importCollectionsRequest } from '@/state/reducers/collections';
-import React, { Ref, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import i18next from 'i18next';
+import { Ref } from 'react';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '../ui/form';
+
+const schema = z.object({
+  file: z
+    .instanceof(File)
+    .refine(
+      (file) => ['application/json', 'application/zip'].includes(file.type),
+      i18next.t('error_unsupported_file_type', { types: '.json, .zip' }),
+    ),
+});
 
 const UploadFileForm = ({ formRef }: { formRef: Ref<HTMLFormElement | null> }) => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
-  const [file, setFile] = useState<File | null>(null);
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-    }
-  };
-
-  const handleImport = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const content = e.target?.result;
-        if (typeof content === 'string') {
-          try {
-            const jsonContent = JSON.parse(content) as object;
-            dispatch(importCollectionRequest(jsonContent));
-          } catch (error) {
-            console.error('Error parsing JSON:', error);
-          }
-        } else if (content instanceof ArrayBuffer) {
-          dispatch(importCollectionsRequest(content));
-        } else {
-          console.error('Unsupported file type');
+  function onSubmit(values: z.infer<typeof schema>) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const content = e.target?.result;
+      if (typeof content === 'string') {
+        try {
+          const jsonContent = JSON.parse(content) as object;
+          dispatch(importCollectionRequest(jsonContent));
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
         }
-      };
-      if (file.name.endsWith('.zip')) {
-        reader.readAsArrayBuffer(file);
+      } else if (content instanceof ArrayBuffer) {
+        dispatch(importCollectionsRequest(content));
       } else {
-        reader.readAsText(file);
+        console.error('Unsupported file type');
       }
+    };
+    if (values.file.name.endsWith('.zip')) {
+      reader.readAsArrayBuffer(values.file);
+    } else {
+      reader.readAsText(values.file);
     }
-  };
+  }
 
   return (
-    <form
-      ref={formRef}
-      onSubmit={handleImport}
-      className='flex flex-col items-center gap-1.5 rounded-2xl border-2 border-gray-200 p-2'
-    >
-      <Label htmlFor='collectionFile'>{t('form_description_select_collection')}</Label>
-      <Input id='collectionFile' type='file' onChange={handleFileChange} />
-      {/* {file && <Button onClick={handleImport}>{t('btn_import')}</Button>} */}
-    </form>
+    <Form {...form}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={form.handleSubmit(onSubmit)}
+        ref={formRef}
+        className='space-y-4'
+      >
+        <FormDescription>{t('form_description_select_collection')}</FormDescription>
+        <FormField
+          control={form.control}
+          name='file'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type='file'
+                  accept='application/json'
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    field.onChange(file);
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 };
 
