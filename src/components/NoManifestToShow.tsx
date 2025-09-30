b77c243c2155b5163e064e2d44ac1a25cb973bec
Jonathan <jonathan.perrinet@epita.fr>
2025-09-30 14:04:14 +0200

    ✨ add Welcome component and update NoManifestToShow for improved user experience

--- src/components/NoManifestToShow.tsx
+++ src/components/NoManifestToShow.tsx
@@ -3,19 +3,25 @@ import { selectHistory } from '@/state/selectors/manifests';
 import { useTranslation } from 'react-i18next';
 import HistoryNav from './HistoryNav';
 import NothingToShow from './NothingToShow';
+import Welcome from './Welcome';
 
 const NoManifestToShow = () => {
   const history = useAppSelector(selectHistory);
   const { t } = useTranslation();
 
   return (
-    <div className='m-2 flex h-full w-full max-w-[600px] flex-col justify-center p-2 text-mezanno-4'>
-      <NothingToShow />
-      {history.length > 0 && (
-        <div className='h-1/2 overflow-auto'>
-          <h4 className='mt-10'>{t('title_recently_opened')}</h4>
-          <HistoryNav />
-        </div>
+    <div className='text-mezanno-4 m-2 flex h-full w-full max-w-[800px] flex-col justify-center p-2'>
+      {history.length > 0 ? (
+        <>
+          <NothingToShow />
+
+          <div className='h-1/2 overflow-auto'>
+            <h4 className='mt-10'>{t('title_recently_opened')}</h4>
+            <HistoryNav />
+          </div>
+        </>
+      ) : (
+        <Welcome />
       )}
     </div>
   );
