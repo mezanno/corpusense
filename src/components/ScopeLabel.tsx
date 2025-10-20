8225e521747fbce63f892ae96308af0ea9628ba8
Jonathan <jonathan.perrinet@epita.fr>
2025-10-20 17:27:09 +0200

    🚸 set link only to a small part of the label

--- src/components/ScopeLabel.tsx
+++ src/components/ScopeLabel.tsx
@@ -2,12 +2,22 @@ import { isAnnotationScope, isCanvasScope, Scope } from '@/data/models/Scope';
 import { useAppSelector } from '@/hooks/hooks';
 import { CorpusenseRoutes } from '@/hooks/useAppNavigation';
 import { selectCollectionById } from '@/state/selectors/collections';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 const ScopeLabel = ({ scope }: { scope: Scope }) => {
+  const { t } = useTranslation();
   const collection = useAppSelector((state) => selectCollectionById(state, scope.collectionId));
   return (
     <div className='flex flex-col'>
+      <div>Collection {collection?.name}</div>
+      {isCanvasScope(scope) && (
+        <div className='break-all' title={scope.canvasId}>
+          Canvas {scope.canvasId}
+        </div>
+      )}
+      {isAnnotationScope(scope) && <div>Annotation {scope.annotationId}</div>}
+
       <Link
         to={`/${CorpusenseRoutes.COLLECTIONS}/${scope.collectionId}${
           isCanvasScope(scope) ? '?canvas=' + scope.canvasId : ''
@@ -15,13 +25,7 @@ const ScopeLabel = ({ scope }: { scope: Scope }) => {
         className='h-full underline'
         title={`Lien vers `}
       >
-        <div>Collection {collection?.name}</div>
-        {isCanvasScope(scope) && (
-          <div className='break-all' title={scope.canvasId}>
-            Canvas {scope.canvasId}
-          </div>
-        )}
-        {isAnnotationScope(scope) && <div>Annotation {scope.annotationId}</div>}
+        ({t('link_open_element')})
       </Link>
     </div>
   );
