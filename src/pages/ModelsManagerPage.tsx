e87345fb17c33b1409f4fc205279cabff5678e62
Jonathan <jonathan.perrinet@epita.fr>
2026-01-28 12:04:16 +0100

    🚸 enhance canvas view toggle functionality and improve component structure

--- src/pages/ModelsManagerPage.tsx
+++ src/pages/ModelsManagerPage.tsx
@@ -2,7 +2,7 @@ import ModelsDashboard from '@/components/models/ModelsDashboard';
 import ModelViewer from '@/components/ModelViewer';
 import QuickCanvasViewer from '@/components/QuickCanvasViewer';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
-import { Container, EyeIcon } from 'lucide-react';
+import { Container, PanelRightClose, PanelRightOpen } from 'lucide-react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -16,25 +16,31 @@ const ModelsManagerPage = () => {
       <h1 className='flex items-center text-2xl font-bold'>
         <Container className='mr-2' /> {t('page_title_models_manager')}
       </h1>
-      <div className='mb-1 flex justify-between'>
-        <button
-          className={`soft-button ${canvasViewVisible ? '' : 'bg-transparent'}`}
-          title={t('btn_toggle_canvas_view')}
-          aria-label={t('btn_toggle_canvas_view')}
-        >
-          <EyeIcon onClick={() => setCanvasViewVisible(!canvasViewVisible)} />
-        </button>
-      </div>
       <ModelsDashboard setSelectedModelId={setSelectedModelId} selectedModelId={selectedModelId} />
       <ResizablePanelGroup direction='horizontal' className='flex-1 space-x-2'>
         <ResizablePanel order={1} id='metadata-panel' className='flex flex-col' minSize={50}>
-          {selectedModelId !== null && <ModelViewer modelId={selectedModelId} />}
+          {selectedModelId !== null && (
+            <div className='flex w-full flex-col'>
+              <div className='flex w-full justify-end'>
+                <button
+                  className={`soft-button ${canvasViewVisible ? '' : 'bg-transparent'} `}
+                  title={t('btn_toggle_canvas_view')}
+                  aria-label={t('btn_toggle_canvas_view')}
+                >
+                  <div onClick={() => setCanvasViewVisible(!canvasViewVisible)}>
+                    {canvasViewVisible ? <PanelRightClose /> : <PanelRightOpen />}
+                  </div>
+                </button>
+              </div>
+              <ModelViewer modelId={selectedModelId} />
+            </div>
+          )}
         </ResizablePanel>
         {canvasViewVisible && (
           <>
             <ResizableHandle withHandle className='w-1 cursor-col-resize bg-dark-slate-gray' />
             <ResizablePanel id='canvas-panel' order={3} minSize={25} className='panel'>
-              <QuickCanvasViewer />
+              {canvasViewVisible && <QuickCanvasViewer />}
             </ResizablePanel>
           </>
         )}
