2b86097ef09adbe0b3e0e4711963142fc5685ac5
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-02-10 14:53:49 +0100

    ✨ implement mergeAnnotations logic to combine overlapping annotations based on distance thresholds

--- src/hooks/data/annotations/useAnnotationMergeAction.tsx
+++ src/hooks/data/annotations/useAnnotationMergeAction.tsx
@@ -1,10 +1,17 @@
-import { Annotation, changeType, duplicateAnnotation, ElementType } from '@/data/models/Annotation';
+import {
+  Annotation,
+  changeType,
+  duplicateAnnotation,
+  ElementType,
+  getDistanceBetweenAnnotations,
+  mergeTwoAnnotations,
+} from '@/data/models/Annotation';
 import {
   getAnnotationLiveRepository,
   getAnnotationRepository,
 } from '@/data/repositories/indexeddb/dbFactory';
 import { useLiveQuery } from 'dexie-react-hooks';
-import { useEffect, useEffectEvent, useMemo, useState } from 'react';
+import { useEffect, useEffectEvent, useMemo } from 'react';
 
 const useAnnotationMergeAction = ({
   collectionId,
@@ -17,7 +24,7 @@ const useAnnotationMergeAction = ({
   const annotationRepository = useMemo(() => getAnnotationRepository(), []);
   const scope = useMemo(() => ({ collectionId, canvasId }), [collectionId, canvasId]);
 
-  const [workingAnnotations, setWorkingAnnotations] = useState<Annotation[]>([]);
+  // const [workingAnnotations, setWorkingAnnotations] = useState<Annotation[]>([]);
   const scopeAnnotations = useLiveQuery(
     annotationLiveRepository.getByScopeAndType(scope, ElementType.TEXT_REGION),
     [scope, annotationLiveRepository],
@@ -39,7 +46,7 @@ const useAnnotationMergeAction = ({
         temps.push(tempAnnotation);
       }
 
-      setWorkingAnnotations(temps);
+      // setWorkingAnnotations(temps);
       await annotationRepository.deleteByScopeAndType({ collectionId }, [ElementType.TEMP]);
       await annotationRepository.addAll(temps);
     })();
@@ -49,9 +56,69 @@ const useAnnotationMergeAction = ({
     onScopeChange();
   }, [scopeAnnotations]);
 
-  const mergeAnnotations = (verticalThreshold: number, horizontalThreshold: number) => {
-    //TODO : implement merge logic
-    console.log('mergeAnnotations', { verticalThreshold, horizontalThreshold });
+  const mergeAnnotations = async (verticalThreshold: number, horizontalThreshold: number) => {
+    if (scopeAnnotations.length > 1) {
+      let annotations = [];
+      for (const annotation of scopeAnnotations) {
+        const tempAnnotation = changeType(duplicateAnnotation(annotation), ElementType.TEMP);
+        annotations.push(tempAnnotation);
+      }
+      annotations.sort(
+        (a, b) => a.target.selector.geometry.bounds.minY - b.target.selector.geometry.bounds.minY,
+      );
+      // console.log(
+      //   'annotations to merge: ',
+      //   annotations.map((a) => a.id.substring(0, 2)),
+      // );
+
+      const mergedAnnotations: Annotation[] = [];
+      const notMerged: Annotation[] = [];
+      let current = annotations.shift()!;
+      do {
+        do {
+          const next = annotations.shift()!;
+          const distance = getDistanceBetweenAnnotations(current, next);
+          // console.log('current: ', current.id.substring(0, 2));
+          // console.log('next: ', next.id.substring(0, 2));
+          // console.log(
+          //   'annotations: ',
+          //   annotations.map((a) => a.id.substring(0, 2)),
+          // );
+          // console.log(
+          //   'notMerged: ',
+          //   notMerged.map((a) => a.id.substring(0, 2)),
+          // );
+          if (
+            Math.abs(distance.vertical) <= verticalThreshold &&
+            Math.abs(distance.horizontal) <= horizontalThreshold
+          ) {
+            // console.log('oui');
+            current = mergeTwoAnnotations(current, next);
+            annotations = [...notMerged, ...annotations];
+          } else {
+            // console.log('non');
+            notMerged.push(next);
+          }
+        } while (annotations.length > 0);
+        mergedAnnotations.push(current);
+        // console.log(
+        //   'merged: ',
+        //   mergedAnnotations.map((a) => a.id.substring(0, 2)),
+        // );
+        current = notMerged.shift()!;
+        annotations = [...notMerged];
+        //clear notMerged for next iteration
+        notMerged.length = 0;
+      } while (annotations.length > 0);
+      mergedAnnotations.push(current, ...annotations);
+      // console.log(
+      //   'mergedAnnotations: ',
+      //   mergedAnnotations.map((a) => a.id.substring(0, 2)),
+      // );
+      await annotationRepository.deleteByScopeAndType({ collectionId }, [ElementType.TEMP]);
+      // setWorkingAnnotations(mergedAnnotations);
+      await annotationRepository.addAll(mergedAnnotations);
+    }
   };
 
   return { mergeAnnotations };
