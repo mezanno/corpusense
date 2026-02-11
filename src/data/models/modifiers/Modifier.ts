7158347fde6ca7c8d3407c535e0d22d2a594dd14
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-02-11 21:13:56 +0100

    🏷️ update Modifier type:  add metadata and description

--- src/data/models/modifiers/Modifier.ts
+++ src/data/models/modifiers/Modifier.ts
@@ -1,15 +1,33 @@
 import z from 'zod';
 import { Annotation } from '../Annotation';
 
+export type FieldMetadata = {
+  label?: string;
+  description?: string;
+  min?: number;
+  max?: number;
+  step?: number;
+};
+
 export abstract class Modifier<TSchema extends z.ZodTypeAny> {
   id: string;
   name: string;
   schema: TSchema;
+  fieldMeta: Record<keyof z.infer<TSchema>, FieldMetadata>;
+  description?: string;
 
-  constructor(id: string, name: string, schema: TSchema) {
+  constructor(
+    id: string,
+    name: string,
+    schema: TSchema,
+    fieldMeta: Record<keyof z.infer<TSchema>, FieldMetadata>,
+    description?: string,
+  ) {
     this.id = id;
     this.name = name;
     this.schema = schema;
+    this.fieldMeta = fieldMeta;
+    this.description = description;
   }
 
   abstract apply(data: Annotation[], values: z.infer<TSchema>): Annotation[];
