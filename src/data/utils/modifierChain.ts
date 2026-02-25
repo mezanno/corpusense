dbd79135a81cf0dadc098f450ebd43d0595e6690
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-02-25 14:32:27 +0100

    ♻️ refactor applyModifiersToScope function for modifier application logic

---
+++ src/data/utils/modifierChain.ts
@@ -0,0 +1,29 @@
+import { AnyModifier } from '@/data/models/modifiers/Modifier';
+import { CanvasScope, CollectionScope } from '@/data/models/Scope';
+import { getAnnotationRepository } from '@/data/repositories/indexeddb/dbFactory';
+
+const applyModifiersToScope = async (
+  modifiers: AnyModifier[],
+  values: Record<string, unknown>,
+  scope: CollectionScope | CanvasScope,
+) => {
+  if (modifiers.length === 0) return;
+
+  const annotationRepository = getAnnotationRepository();
+  const scopeAnnotations = await annotationRepository.getByScope(scope);
+
+  let annotations = [...scopeAnnotations];
+  modifiers.forEach((modifier) => {
+    const modifierValues = values[modifier.id];
+    if (modifierValues !== undefined) {
+      const rawValues = values[modifier.id] ?? {};
+      const parsedValues = modifier.schema.parse(rawValues);
+      annotations = modifier.apply(annotations, parsedValues);
+    }
+  });
+
+  await annotationRepository.deleteByIds(scopeAnnotations.map((a) => a.id));
+  await annotationRepository.addAll(annotations);
+};
+
+export { applyModifiersToScope };
