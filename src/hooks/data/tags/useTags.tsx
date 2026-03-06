e1e5972a5439471bcbe44ffa08fac718b2c04e50
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-03-06 15:11:30 +0100

    🐛 fix useMemo usage and improve error logging in useTags hook

--- src/hooks/data/tags/useTags.tsx
+++ src/hooks/data/tags/useTags.tsx
@@ -4,21 +4,21 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import { useCallback, useMemo } from 'react';
 
 export const useTags = () => {
-  const tagLiveRepository = useMemo(getTagLiveRepository, []);
-  const tagRepository = useMemo(getTagRepository, []);
+  const tagLiveRepository = useMemo(() => getTagLiveRepository(), []);
+  const tagRepository = useMemo(() => getTagRepository(), []);
 
   const tags = useLiveQuery(tagLiveRepository.getAll(), [tagLiveRepository], [] as Tag[]);
 
   const getTagsByIds = useCallback(
     (ids: string[]) => ids?.map((id) => tags.find((t) => t.id === id)).filter(Boolean) ?? [],
-    [],
+    [tags],
   );
 
   const createNewTag = async (newTag: Tag) => {
     try {
       await tagRepository.add(newTag);
     } catch (error) {
-      console.error(error);
+      console.warn(error);
     }
   };
 
