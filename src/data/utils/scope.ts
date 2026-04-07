6440a9f22c18b2e45eb51d7b39aeb068f0f7e6b2
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-04-07 13:55:19 +0200

    🔥 remove debug logging

--- src/data/utils/scope.ts
+++ src/data/utils/scope.ts
@@ -2,8 +2,6 @@ import { Scope } from '../models/Scope';
 import { getCollectionRepository } from '../repositories/indexeddb/dbFactory';
 
 const contains = async (scope: Scope, value: string) => {
-  console.log(scope, ' contains ', value);
-
   const collectionRepository = getCollectionRepository();
   try {
     const collection = await collectionRepository.getById(scope.collectionId);
