904b3c826dc119b2c9131d59019a1b49c2b50d44
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-04-15 15:06:27 +0200

    ⚰️ remove unused computeScopeKey import and console log from useCollectionLlmStatus

--- src/hooks/data/collections/useCollectionLlmStatus.tsx
+++ src/hooks/data/collections/useCollectionLlmStatus.tsx
@@ -1,12 +1,9 @@
 import { getWorkerLiveRepository } from '@/data/repositories/indexeddb/dbFactory';
-import { computeScopeKey } from '@/data/repositories/indexeddb/utils';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { useMemo } from 'react';
 
 const useCollectionLlmStatus = ({ collectionId }: { collectionId: string }) => {
   const workerLiveRepository = useMemo(() => getWorkerLiveRepository(), []);
-
-  console.log(computeScopeKey({ collectionId }));
 
   const hasLlmResult = useLiveQuery(
     workerLiveRepository.hasResult({ collectionId }, 'mistral'),
