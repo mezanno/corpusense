c4d28eaa350d14e0e4137db73ff06237a7937468
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-04-15 17:22:22 +0200

    🚸 enhance LlmStatus and useCollectionLlmStatus: integrate ReactJsonView for structured data display and improve data fetching logic

--- src/hooks/data/collections/useCollectionLlmStatus.tsx
+++ src/hooks/data/collections/useCollectionLlmStatus.tsx
@@ -1,9 +1,29 @@
-import { getWorkerLiveRepository } from '@/data/repositories/indexeddb/dbFactory';
+import { workerPlugins } from '@/App';
+import {
+  getResultRepository,
+  getWorkerLiveRepository,
+  getWorkerRepository,
+} from '@/data/repositories/indexeddb/dbFactory';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { useMemo } from 'react';
 
 const useCollectionLlmStatus = ({ collectionId }: { collectionId: string }) => {
   const workerLiveRepository = useMemo(() => getWorkerLiveRepository(), []);
+
+  const getResultData = async () => {
+    const workerRepository = getWorkerRepository();
+    const worker = await workerRepository.getByNameAndScope('mistral', { collectionId });
+    if (!worker) return;
+
+    const saga = workerPlugins[worker.name];
+
+    const resultRepository = getResultRepository();
+    const results = await resultRepository.getAllByWorkerId(worker.id);
+
+    if (saga?.extractData) {
+      return await saga.extractData(results);
+    }
+  };
 
   const hasLlmResult = useLiveQuery(
     workerLiveRepository.hasResult({ collectionId }, 'mistral'),
@@ -11,7 +31,7 @@ const useCollectionLlmStatus = ({ collectionId }: { collectionId: string }) => {
     false,
   );
 
-  return hasLlmResult;
+  return { hasLlmResult, getResultData };
 };
 
 export default useCollectionLlmStatus;
