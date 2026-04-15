4623ea8d436b62efb63a8c99778557bcb7877a3b
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-04-15 14:48:07 +0200

    🚸 refactor OcrStatus: integrate IconWithTooltip for improved tooltip functionality and handle click event

--- src/components/collectionPage/OcrStatus.tsx
+++ src/components/collectionPage/OcrStatus.tsx
@@ -1,18 +1,31 @@
+import { generateTextForCollection } from '@/data/utils/export';
 import useCollectionOcrStatus from '@/hooks/data/collections/useCollectionOcrStatus';
 import { BookA } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import { IconWithTooltip } from '../IconWithTooltip';
+import { useAlertDialogContext } from '../reducers/useAlertDialogContext';
 
 const OcrStatus = ({ collectionId }: { collectionId: string }) => {
   const { t } = useTranslation();
+  const { openDialog } = useAlertDialogContext();
   const hasOcr = useCollectionOcrStatus({ collectionId });
 
   if (!hasOcr) {
     return null;
   }
+
+  const handleClick = async () => {
+    const text = await generateTextForCollection(collectionId);
+    openDialog({
+      title: t('title_ocr_text'),
+      children: <div className='whitespace-pre-line'>{text}</div>,
+    });
+  };
+
   return (
-    <div title={t('info_ocr_available')} className='flex items-center gap-1'>
+    <IconWithTooltip tooltip={t('info_ocr_available')} onClick={() => void handleClick()}>
       <BookA />
-    </div>
+    </IconWithTooltip>
   );
 };
 
